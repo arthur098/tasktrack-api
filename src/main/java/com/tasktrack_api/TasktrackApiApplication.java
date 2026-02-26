d24bdce8c442708ec248439279caa27bc6455eb8
Arthur Oliveira Rodrigues <arroiseeu@gmail.com>
2026-02-25 23:45:17 -0300

    Adding validation configurations;

--- src/main/java/com/tasktrack_api/TasktrackApiApplication.java
+++ src/main/java/com/tasktrack_api/TasktrackApiApplication.java
@@ -2,10 +2,18 @@ package com.tasktrack_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingEntityCallback;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class TasktrackApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasktrackApiApplication.class, args);
+	}
+
+	@Bean
+	public ValidatingEntityCallback validatingEntityCallback(LocalValidatorFactoryBean validator) {
+		return new ValidatingEntityCallback(validator);
 	}
 }
